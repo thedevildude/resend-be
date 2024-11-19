7ce988c824a735d5c5851fbdb014c3a9d91d382e
Devdeep Ghosh <devdeep@capitalcompute.com>
2024-11-20 00:36:02 +0530

    pick fixed

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,7 +1,7 @@
 import httpStatus from "http-status";
 import ApiError from "../utils/ApiError";
 import { NextFunction, Request, Response } from "express";
-import pick from "../utils/Pick";
+import pick from "../utils/pick";
 import Joi from "joi";
 
 const validate =
